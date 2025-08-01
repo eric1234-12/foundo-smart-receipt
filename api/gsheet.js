a17071d84bae1d188edc5d27f8567afd74d92d9d
eric1234-12 <eric@foundo.com>
2025-08-01 15:38:02 +0800

    Update gsheet.js

--- api/gsheet.js
+++ api/gsheet.js
@@ -25,13 +25,25 @@ export default async function handler(req, res) {
     }
     imageUrl = data.imageUrl;
   }
+console.log("invoice":invoice);
+  console.log("amount":amount);
+  console.log("amount":amount);
+  console.log("date":date);
+  console.log("paid":paid);
+    console.log("payer":payer);
+    console.log("note":note);
 
+    console.log("brand":brand);
+    console.log("category":category);
+    console.log("timestamp":33);
+     console.log("imageUrl":imageUrl);
+  
   const sheetRes = await fetch('https://script.google.com/macros/s/AKfycbxebo8fn4PVzl1j-E933KfyOMXCKWLFf1FdZ4iWTwGJC4Yeh5-TapEreZouobT_Y2fn/exec',{
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       appendOnly: true,
-      invoice, date, amount, product, paid, payer, note, brand, category,
+      invoice, date, amount, amount, paid, payer, note, brand, category,
       timestamp: new Date().toLocaleString("en-US", { timeZone: "Asia/Kuala_Lumpur" }),
       imageUrl
     })
