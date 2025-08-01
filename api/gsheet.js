eab13ba3d01c165646e8d87c2156b3bde2ef3179
eric1234-12 <eric@foundo.com>
2025-08-01 15:43:06 +0800

    Update gsheet.js

--- api/gsheet.js
+++ api/gsheet.js
@@ -25,18 +25,18 @@ export default async function handler(req, res) {
     }
     imageUrl = data.imageUrl;
   }
-console.log("invoice":invoice);
-  console.log("amount":amount);
-  console.log("amount":amount);
-  console.log("date":date);
-  console.log("paid":paid);
-    console.log("payer":payer);
-    console.log("note":note);
+console.log("invoice",invoice);
+  console.log("amount",amount);
+  console.log("amount",amount);
+  console.log("date",date);
+  console.log("paid",paid);
+    console.log("payer",payer);
+    console.log("note",note);
 
-    console.log("brand":brand);
-    console.log("category":category);
-    console.log("timestamp":33);
-     console.log("imageUrl":imageUrl);
+    console.log("brand",brand);
+    console.log("category",category);
+    console.log("timestamp",33);
+     console.log("imageUrl",imageUrl);
   
   const sheetRes = await fetch('https://script.google.com/macros/s/AKfycbxebo8fn4PVzl1j-E933KfyOMXCKWLFf1FdZ4iWTwGJC4Yeh5-TapEreZouobT_Y2fn/exec',{
     method: 'POST',
